d71499d61f9d0dec034401861f2abcd34d71dec4
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-03-31 14:36:43 +0300

    Initial commit

---
+++ api/src/main/java/eu/mikroskeem/orion/api/configuration/Test.java
@@ -0,0 +1,12 @@
+package eu.mikroskeem.orion.api.configuration;
+
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * @author Mark Vainomaa
+ */
+public class Test {
+    static {
+        ConfigFactory.
+    }
+}
