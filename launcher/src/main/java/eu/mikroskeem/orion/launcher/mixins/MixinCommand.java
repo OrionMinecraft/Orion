fbcff20aaf2c35aa02095732b0dd4d190bbf5fd0
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-03 07:15:45 +0300

    Patch default permission message (WIP)

--- launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinCommand.java
+++ launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinCommand.java
@@ -1,6 +1,7 @@
 package eu.mikroskeem.orion.launcher.mixins;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -8,10 +9,25 @@ import org.spongepowered.asm.mixin.Shadow;
  * @author Mark Vainomaa
  */
 @Mixin(Command.class)
-public class MixinCommand {
+public abstract class MixinCommand {
+    @Shadow(remap = false) private String permission;
     @Shadow(remap = false) private String name;
+    @Shadow(remap = false) private String permissionMessage;
+    @Shadow(remap = false) public abstract boolean testPermissionSilent(CommandSender target);
 
-    public String getPermissionMessage(){
-        return "No command " + this.name + "for you.";
+    public boolean testPermission(CommandSender target) {
+        if (testPermissionSilent(target)) {
+            return true;
+        }
+
+        if (permissionMessage == null) {
+            target.sendMessage("no command " + name + " for you");
+        } else if (permissionMessage.length() != 0) {
+            for (String line : permissionMessage.replace("<permission>", permission).split("\n")) {
+                target.sendMessage(line);
+            }
+        }
+
+        return false;
     }
 }
