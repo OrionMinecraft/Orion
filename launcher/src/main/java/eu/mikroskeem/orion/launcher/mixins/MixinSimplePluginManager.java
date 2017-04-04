84519d49cc22a6741da4e28598c628c2fe3d1b73
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-04 19:53:45 +0300

    Proper plugin unloading

--- launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinSimplePluginManager.java
+++ launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinSimplePluginManager.java
@@ -1,11 +1,15 @@
 package eu.mikroskeem.orion.launcher.mixins;
 
 import eu.mikroskeem.orion.api.plugin.PluginManager;
+import eu.mikroskeem.orion.internal.interfaces.ExposedJavaPluginLoader;
 import lombok.SneakyThrows;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.SimplePluginManager;
+import org.bukkit.plugin.java.JavaPluginLoader;
+import org.bukkit.plugin.java.PluginClassLoader;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,6 +18,7 @@ import java.io.IOException;
 import java.net.URLClassLoader;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -23,7 +28,6 @@ import java.util.Map;
 public abstract class MixinSimplePluginManager implements PluginManager {
     @Shadow(remap = false) @Final private Map<String, Plugin> lookupNames;
     @Shadow(remap = false) @Final private List<Plugin> plugins;
-
     @Shadow(remap = false) @Final private SimpleCommandMap commandMap;
 
     @Override
@@ -39,28 +43,38 @@ public abstract class MixinSimplePluginManager implements PluginManager {
     @Override
     @SneakyThrows(IOException.class)
     public void unloadPlugin(Plugin plugin){
+        String pluginName = plugin.getName().toLowerCase(Locale.ENGLISH);
+
         /* Disable */
         disablePlugin(plugin);
 
-        /* Remove */
-        lookupNames.remove(plugin.getName(), plugin);
+        /* Remove from lists */
+        lookupNames.remove(pluginName, plugin);
         plugins.remove(plugin);
 
         /* Unregister commands */
         new HashMap<>(commandMap.getKnownCommands()).values().stream()
                 .filter(c -> c instanceof PluginCommand)
                 .map(c -> (PluginCommand)c)
-                .filter(c -> c.getPlugin().equals(plugin))
+                .filter(c -> c.getPlugin().getName().toLowerCase(Locale.ENGLISH).equals(pluginName))
                 .forEach(c -> {
                     c.unregister(commandMap);
                     commandMap.getKnownCommands().remove(c.getName(), c);
-                    commandMap.getKnownCommands()
-                            .remove(plugin.getName() + ":" + c.getName(), c); // Fallback prefix
+                    commandMap.getKnownCommands().remove(pluginName + ":" + c.getName());
         });
 
-        /* Unload classloader */
+        /* Unregister listeners for sure */
+        HandlerList.unregisterAll(plugin);
+
+        /* Close classloader and trigger garbage collector */
         ClassLoader classLoader = plugin.getClass().getClassLoader();
-        if(classLoader instanceof URLClassLoader) ((URLClassLoader) classLoader).close();
+        if(plugin.getPluginLoader() instanceof JavaPluginLoader) {
+            ExposedJavaPluginLoader jpl = (ExposedJavaPluginLoader)plugin.getPluginLoader();
+            PluginClassLoader pcl = (PluginClassLoader)classLoader;
+            if(jpl.getLoaders().contains(pcl))
+                jpl.getLoaders().remove(pcl);
+        }
+        if (classLoader instanceof URLClassLoader) ((URLClassLoader) classLoader).close();
         System.gc();
     }
 }
