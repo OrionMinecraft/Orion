8ceab8b7959c2f66c736deda3d10b3b22b5c40c6
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-08 23:56:27 +0300

    Do not replace whole method, use redirection instead

--- launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinVersionCommand.java
+++ launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinVersionCommand.java
@@ -1,13 +1,13 @@
 package eu.mikroskeem.orion.launcher.mixins;
 
 import com.google.common.collect.ImmutableList;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.VersionCommand;
-import org.bukkit.plugin.Plugin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * @author Mark Vainomaa
@@ -17,47 +17,14 @@ public abstract class MixinVersionCommand extends Command {
     public MixinVersionCommand(){super(null, null, null, ImmutableList.of()); }
 
     @Shadow(remap = false) protected abstract void sendVersion(CommandSender sender);
-    @Shadow(remap = false) protected abstract void describeToSender(Plugin plugin, CommandSender sender);
 
-    public boolean execute(CommandSender sender, String currentAlias, String[] args) {
-        if (!testPermission(sender)) return true;
-
-        if (args.length == 0) {
-            sender.sendMessage("This server is running " + Bukkit.getName() + " version " + Bukkit.getVersion() + " (Implementing API version " + Bukkit.getBukkitVersion() + ")");
-            sendVersion(sender);
-            sender.sendMessage("§7Server uses §bOrion Launcher §7made with §c\u2764 §7by §c§lmikroskeem");
-        } else {
-            StringBuilder name = new StringBuilder();
-
-            for (String arg : args) {
-                if (name.length() > 0) {
-                    name.append(' ');
-                }
-
-                name.append(arg);
-            }
-
-            String pluginName = name.toString();
-            Plugin exactPlugin = Bukkit.getPluginManager().getPlugin(pluginName);
-            if (exactPlugin != null) {
-                describeToSender(exactPlugin, sender);
-                return true;
-            }
-
-            boolean found = false;
-            pluginName = pluginName.toLowerCase(java.util.Locale.ENGLISH);
-            for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
-                if (plugin.getName().toLowerCase(java.util.Locale.ENGLISH).contains(pluginName)) {
-                    describeToSender(plugin, sender);
-                    found = true;
-                }
-            }
-
-            if (!found) {
-                sender.sendMessage("This server is not running any plugin by that name.");
-                sender.sendMessage("Use /plugins to get a list of plugins.");
-            }
-        }
-        return true;
+    @Redirect(remap = false,
+            method = "execute(Lorg/bukkit/command/CommandSender;Ljava/lang/String;[Ljava/lang/String;)Z",
+            at = @At(remap = false, value = "INVOKE",
+            target = "Lorg/bukkit/command/defaults/VersionCommand;sendVersion(Lorg/bukkit/command/CommandSender;)V"
+    ))
+    public void proxySendVersion(VersionCommand versionCommand, CommandSender sender) {
+        sendVersion(sender);
+        sender.sendMessage("§7Server uses §bOrion Launcher §7made with §c\u2764 §7by §c§lmikroskeem");
     }
 }
