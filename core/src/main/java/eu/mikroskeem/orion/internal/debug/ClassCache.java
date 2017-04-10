98aa8be4f290506f44bf4997069b635091380320
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-10 18:32:21 +0300

    Fix Bukkit event class scanning

--- core/src/main/java/eu/mikroskeem/orion/internal/debug/ClassCache.java
+++ core/src/main/java/eu/mikroskeem/orion/internal/debug/ClassCache.java
@@ -13,14 +13,20 @@ import java.util.List;
 public class ClassCache {
     private final static List<Class<? extends Event>> availableEventClasses = new ArrayList<>();
 
+    @SuppressWarnings("unchecked")
+    private static void add(Class<?> clazz){
+        /* Note: Mixin makes Bukkit event classes compatible with my classes */
+        availableEventClasses.add((Class<? extends Event>)clazz);
+    }
+
     public static List<Class<? extends Event>> getEventClasses() {
         if(availableEventClasses.size() == 0) {
             synchronized (ClassCache.class) {
-                List<Class<? extends Event>> found = new ArrayList<>();
                 new FastClasspathScanner("")
                         .addClassLoader(Bukkit.class.getClassLoader())
-                        .matchClassesImplementing(Event.class, found::add).scan();
-                availableEventClasses.addAll(found);
+                        .matchClassesImplementing(Event.class, ClassCache::add)
+                        .matchSubclassesOf(org.bukkit.event.Event.class, ClassCache::add)
+                        .scan();
             }
         }
         return availableEventClasses;
