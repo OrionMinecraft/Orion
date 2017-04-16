0fda7c76e264e128d838dd354028f784ac7009cc
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-16 14:08:52 +0300

    Move configuration initialization into core

--- core/src/main/java/eu/mikroskeem/orion/OrionServerCore.java
+++ core/src/main/java/eu/mikroskeem/orion/OrionServerCore.java
@@ -5,12 +5,15 @@ import eu.mikroskeem.orion.api.server.Configuration;
 import eu.mikroskeem.orion.impl.configuration.HoconFileConfiguration;
 import eu.mikroskeem.orion.internal.debug.sentry.SentryReporter;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 /**
  * @author Mark Vainomaa
  */
+@Slf4j
 public class OrionServerCore implements OrionServer {
     @Getter private static OrionServer instance;
     @Getter private Configuration configuration;
@@ -19,6 +22,13 @@ public class OrionServerCore implements OrionServer {
     public OrionServerCore(){
         instance = this;
         configuration = new HoconFileConfiguration(Paths.get(".", "orion.cfg"));
+        try {
+            configuration.load();
+            configuration.save();
+        } catch (IOException e){
+            log.error("Failed to load Orion configuration!", e);
+        }
+
         sentryReporter = new SentryReporter(this);
     }
 }
--- launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinBukkit.java
+++ launcher/src/main/java/eu/mikroskeem/orion/launcher/mixins/MixinBukkit.java
@@ -2,16 +2,12 @@ package eu.mikroskeem.orion.launcher.mixins;
 
 import eu.mikroskeem.orion.OrionServerCore;
 import eu.mikroskeem.orion.api.Orion;
-import eu.mikroskeem.orion.api.OrionServer;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.io.IOException;
-import java.util.logging.Level;
 
 /**
  * @author Mark Vainomaa
@@ -20,15 +16,7 @@ import java.util.logging.Level;
 public abstract class MixinBukkit {
     @Inject(remap = false, method = "setServer(Lorg/bukkit/Server;)V", at = @At(value = "HEAD", remap = false))
     private static void onSetServer(Server server, CallbackInfo callbackInfo){
-        OrionServer orionServer = new OrionServerCore();
-        Orion.setServer(orionServer);
-        try {
-            orionServer.getConfiguration().load();
-            orionServer.getConfiguration().save();
-        } catch (IOException e){
-            server.getLogger().log(Level.SEVERE, "Failed to load Orion configuration!", e);
-
-        }
+        Orion.setServer(new OrionServerCore());
         server.getLogger().info("This server is launched with OrionLauncher, made with \u2764 by mikroskeem");
     }
 }
