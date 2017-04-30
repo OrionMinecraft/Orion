c97f57bdfc7d69a71180a6b2ea82f5231081fe17
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-04-30 15:44:17 +0300

    Use Orion world spawnpoint on respawn

--- mixins/src/main/java/eu/mikroskeem/orion/mod/mixins/spawnpoint/MixinPlayerList.java
+++ mixins/src/main/java/eu/mikroskeem/orion/mod/mixins/spawnpoint/MixinPlayerList.java
@@ -1,41 +1,30 @@
 package eu.mikroskeem.orion.mod.mixins.spawnpoint;
 
-import net.minecraft.server.v1_11_R1.EntityPlayer;
+import eu.mikroskeem.orion.internal.interfaces.OrionWorldData;
 import net.minecraft.server.v1_11_R1.PlayerList;
-import net.minecraft.server.v1_11_R1.WorldServer;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_11_R1.CraftWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.HashSet;
-import java.util.Set;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * @author Mark Vainomaa
  */
 @Mixin(PlayerList.class)
 public abstract class MixinPlayerList {
-    /* TODO: finish this
-    private final Set<EntityPlayer> orion$redirectWorldSpawnForPlayers = new HashSet<>();
+    private final static String MOVE_TO_WORLD = "moveToWorld(Lnet/minecraft/server/v1_11_R1/EntityPlayer;IZ" +
+            "Lorg/bukkit/Location;Z)Lnet/minecraft/server/v1_11_R1/EntityPlayer;";
 
-    @Inject(remap = false, method = "moveToWorld(Lnet/minecraft/server/v1_11_R1/EntityPlayer;IZ" +
-            "Lorg/bukkit/Location;Z)" +
-            "Lnet/minecraft/server/v1_11_R1/EntityPlayer;",
+    @Redirect(remap = false, method = MOVE_TO_WORLD,
             at = @At(remap = false,
-            shift = At.Shift.AFTER,
-            value = "INVOKE",
-            //target = "Lnet/minecraft/server/v1_11_R1/WorldServer;" +
-            //        "getSpawn()Lnet/minecraft/server/v1_11_R1/BlockPosition;"
-            //target = "Ljava/util/List;get(I)Ljava/lang/Object;"
-            target = "Lorg/bukkit/Location;<init>(Lorg/bukkit/World;DDD)Lorg/bukkit/Location;"
-
+            value = "NEW",
+            target = "(Lorg/bukkit/World;DDD)Lorg/bukkit/Location;",
+            ordinal = 1,
+            args = "class=org/bukkit/Location"
     ))
-    public void onGetWorldSpawn(EntityPlayer entityplayer, int i, boolean flag, Location location,
-                                      boolean avoidSuffocation, CallbackInfoReturnable<EntityPlayer> cb) {
-        new Throwable("PlayerList -> (after) new Location()").printStackTrace();
-        //orion$redirectWorldSpawnForPlayers.add(entityplayer);
-    }*/
+    public Location newLocation(World world, double x, double y, double z) {
+        return ((OrionWorldData) ((CraftWorld) world).getHandle().worldData).getSpawnpoint();
+    }
 }
