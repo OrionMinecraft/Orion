f80e0a70d952a6df3b5cc2412d28f78de8b25950
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-05-04 07:48:09 +0300

    More documentation

--- api/src/main/java/eu/mikroskeem/orion/api/entities/Player.java
+++ api/src/main/java/eu/mikroskeem/orion/api/entities/Player.java
@@ -1,9 +1,23 @@
 package eu.mikroskeem.orion.api.entities;
 
+import eu.mikroskeem.orion.api.server.Configuration;
+
 /**
  * @author Mark Vainomaa
  */
 public interface Player extends org.bukkit.entity.Player {
+    /**
+     * Get the last time when player was active
+     *
+     * @return Seconds from Unix epoch
+     */
     long getLastActiveTime();
+
+    /**
+     * Shows if player is away or not, value is set by comparing
+     * value in {@link Configuration.PlayerConfiguration#getMillisecondsUntilToMarkPlayerAway()}
+     *
+     * @return Whether player is away or not
+     */
     boolean isAway();
 }
--- api/src/main/java/eu/mikroskeem/orion/api/server/Configuration.java
+++ api/src/main/java/eu/mikroskeem/orion/api/server/Configuration.java
@@ -10,39 +10,59 @@ public interface Configuration {
             "§cI'm sorry, but you do not have permission to perform this command. " +
                     "Please contact the server administrators if you believe that this is in error.";
 
+    /**
+     * Save configuration
+     *
+     * @throws IOException If configuration backend fails to save
+     */
     default void save() throws IOException {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Load configuration
+     *
+     * @throws IOException If configuration backend fails to load
+     */
     default void load() throws IOException {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Reload configuration
+     *
+     * @throws IOException If configuration backend fails to reload
+     */
     default void reload() throws IOException {
         throw new UnsupportedOperationException();
     }
 
+    /** Get messages subconfiguration */
     default Messages getMessages() {
         throw new UnsupportedOperationException();
     }
 
+    /** Get debug subconfiguration */
     default Debug getDebug() {
         throw new UnsupportedOperationException();
     }
 
+    /** Get commands subconfiguration */
     default Commands getCommands() {
         throw new UnsupportedOperationException();
     }
 
+    /** Get sentry subconfiguration */
     default Sentry getSentry() {
         throw new UnsupportedOperationException();
     }
 
+    /** Get player subconfiguration */
     default PlayerConfiguration getPlayerConfiguration() {
         throw new UnsupportedOperationException();
     }
 
-    /* Debug subconfiguration */
+    /** Debug subconfiguration */
     interface Debug {
         boolean isEventDumpingAllowed();
         boolean isScriptEventHandlerAllowed();
@@ -51,22 +71,22 @@ public interface Configuration {
         String getHastebinUrl();
     }
 
-    /* Messages subconfiguration */
+    /** Messages subconfiguration interface */
     interface Messages {
         String getCommandPermissionDeniedMessage();
     }
 
-    /* Commands subconfiguration */
+    /** Commands subconfiguration interface */
     interface Commands {
         boolean isOverridingPluginCommandPermissionDeniedMessageEnabled();
     }
 
-    /* Sentry subconfiguration */
+    /** Sentry subconfiguration interface */
     interface Sentry {
         String getSentryDSN();
     }
 
-    /* Player subconfiguration */
+    /** Player subconfiguration interface */
     interface PlayerConfiguration {
         long getMillisecondsUntilToMarkPlayerAway();
         boolean isPlayerDataSavingDisabled();
